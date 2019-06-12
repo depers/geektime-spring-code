3549b42e6893f7053ca04c47d7fb4965043b21e0
depers <dev_fengxiao@163.com>
2019-06-12 11:00:04 +0800

    12. errorcode-demo

--- 11/declarative-transaction-demo/src/main/java/geektime/spring/data/declarativetransactiondemo/FooServiceImpl.java
+++ 11/declarative-transaction-demo/src/main/java/geektime/spring/data/declarativetransactiondemo/FooServiceImpl.java
@@ -24,6 +24,7 @@ public class FooServiceImpl implements FooService {
     }
 
     @Override
+    //@Transactional(rollbackFor = RollbackException.class)
     public void invokeInsertThenRollback() throws RollbackException {
         insertThenRollback();
     }
