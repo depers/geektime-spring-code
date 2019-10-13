d6220d3fa9a3d63b1b18c79d4576b9ad02f0a2b0
depers <dev_fengxiao@163.com>
2019-10-13 16:31:16 +0800

    chapter 7

--- Chapter 6/stream-demo/src/main/java/geektime/spring/faq/Demo.java
+++ Chapter 6/stream-demo/src/main/java/geektime/spring/faq/Demo.java
@@ -12,7 +12,9 @@ public class Demo {
                 .forEach(System.out::println);
 
         Arrays.stream(new String[] { "s1", "s2", "s3" })
+                // map是将str放到了list中
                 .map(s -> Arrays.asList(s))
+                // flat则是将list中的元素取出然后拍平
                 .flatMap(l -> l.stream())
                 .forEach(System.out::println);
     }
